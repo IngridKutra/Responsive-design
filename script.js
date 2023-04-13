18ed261fbe197f0a97fbfc4cdecc5c0009f705ea
Ingrid <ingrid.lia.testing@gmail.com>
2023-04-13 20:49:16 +0200

    ok

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Dropdown menu here
 function myFunction() {
 
     document.getElementById("myDropdown").classList.toggle("show");
@@ -19,14 +20,50 @@ function myFunction() {
 
 }
 
+  //comment section here
 function commentFunction() {
 
 var x = document.getElementById("inputText").value;
 document.getElementById("result").innerHTML = x;
 }
 
+
+  //Alert window here
 function alertFunction() {
 
   alert("Tack! Jag återkomma inom 2 dagar:)");
   window.location.href = "/index.html";
   }
+
+  
+  //Showslides here
+  var slideIndex = 1;
+function runSlides(){  
+  showSlides(slideIndex);
+}
+  
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+ 
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
